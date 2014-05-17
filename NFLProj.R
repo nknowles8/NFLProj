f3d1cebdae71cb1d7a068469a29ad6c1624f3122
selwonka <s3452604@student.rmit.edu.au>
2014-05-17 19:06:01 +1000

    Update NFLProj.R
    changed directions, now performing a 1-time edit of the .csv files

--- NFLProj.R
+++ NFLProj.R
@@ -1,91 +1,54 @@
-getNFLData <- function(year){
-      setwd("H:/Docs/Sports/Project")
-      raw <- read.csv(paste(as.character(year), "_nfl_pbp_data.csv", sep = ""))
-}
-
-addConvert <- function(data){
-      data[,"convert"] <- FALSE
+prepData <- function(read_dir = "H:/Docs/Sports/Project/raw_data", write_dir = "H:/Docs/Sports/Project/wd", years = c(2002:2009, 2011,2012)){
+      setwd(read_dir)
       
-      for(i in 1:(nrow(data) - 1)){
+      for (i in years){
+            year_data <- read.csv(paste(as.character(i), "_nfl_pbp_data.csv", sep = ""))
+            year_data <- subset(year_data, select = -c(season,qtr))     #these are redundant columns
+            year_data[is.na(year_data)] <- 0                            #replace NAs with 0s -- makes evaluating boolean expressions easy
+            year_data <- addPType(year_data)
+            year_data <- addConvert(year_data)
             
-            if(is.na(data[i + 1, "down"])){
-                  data[i + 1, "down"] <-5
-            }
-            
-            if(((data[i + 1,"down"] == 1) | (data[i + 1, "offscore"] > data[i, "offscore"])) & (data[i, "off"] == data[i + 1, "off"]) & (is.na(data[i,"down"]) == FALSE)){
-                  data[i,"convert"] <- TRUE
-            }
+            write.csv(year_data, paste(as.character(i), "_nfl_pbp_dataP.csv", sep=""))
       }
 }
 
-trimNFLData <- function(data, off.spec = "", def.spec = "", five_min = FALSE, score_diff = 21){
-      #this function trims the dataset according to user specifications
-      #data is the dataset to be trimmed; field.spec is a string that equals "middle", "own20", "redzone", or "all"; off.spec and def.spec
-      #are 2 to 3 letter strings corresponding to each team; down.spect is a vector of numbers describing downs to include; distance.spec
-      #is a vector of numbers describing the acceptable values for "togo"; five_min is T/F specifying whether the data in the last 5 min
-      #of each half should be excluded; score_diff is an integer threshold that excludes score differences above the threshold
+addPType <- function(data){                                             #this function classifies each play as "Run", "Pass", or "Other"
       
-      data[,"convert"] <- FALSE
-      yard.max <- 80
-      yard.min <- 20
-      
-      data[1, "down"] <- 5
-      
-      for(i in 1:(nrow(data) - 1)){
-            
-            if(is.na(data[i + 1, "down"])){
-                  data[i + 1, "down"] <-5
-            }
-            
-            if(((data[i + 1,"down"] == 1) | (data[i + 1, "offscore"] > data[i, "offscore"])) & (data[i, "off"] == data[i + 1, "off"]) & (is.na(data[i,"down"]) == FALSE)){
-                  data[i,"convert"] <- TRUE
-            }
-      }
-      
-      if(off.spec != ""){ #trim data based on offense
-            data <- subset(data, off == off.spec)
-      }
-      
-      if(def.spec != ""){ #trim data based on defense
-            data <- subset(data, def == def.spec)
-      }
-      
-      
-      if (five_min == TRUE){ 
-            data <- subset(data, (min %in% c(5:30, 35:60)) | (qtr %in% c(1,3)))
-      }
-      
-      data <- subset(data,(abs(offscore-defscore)<=score_diff), select = -c(season,qtr))
-}
- 
-classifyNFLData <-function(data){
-      data[,"Type"] <- NA
-      desc <- data[,"description"]
+      data[,"pType"] <- "Other"
+      desc <- data[,"description"]                                      #seems to marginally decrease computation time
       
       for(i in 1:nrow(data)){
             if((grepl("tackle", desc[i])>0)|(grepl("end", desc[i])>0)|(grepl("up the middle", desc[i])>0)|(grepl("guard", desc[i])==1)&(grepl("scramble", desc[i])==0)){
-                  data[i,"Type"] <- "Run"
+                  data[i,"pType"] <- "Run"
             } else if((grepl("pass", desc[i])>0)|(grepl("sacked", desc[i])>0)|(grepl("scramble", desc[i])>0)){
-                  data[i,"Type"] <- "Pass"
-            } else{
-                  data[i,"Type"] <- "Other"
-            }
+                  data[i,"pType"] <- "Pass"
+            } 
       }
       return(data)
 }
 
-prepSummary <- function(years = 2012, directory="H:/Docs/Sports/Project", off.spec = "", def.spec = ""){
+addPType2 <- function(data){                                             #this function classifies each play as "Run", "Pass", or "Other"
       
-      #this takes about 4 min to load 1 year of league data and 2 min for 1 yr of team data, 6 min for 2 years of team data
-  
-      setwd(directory)
-      data <- NULL
+      data[,"pType"] <- "Other"
       
-      for (i in years){
-            yeardata <- read.csv(paste(as.character(i), "_nfl_pbp_data.csv", sep = ""))
-            yeardata <- trimNFLData(yeardata, off.spec, def.spec, five_min, score_diff)
-            data <- rbind(data, yeardata)
+      for(i in 1:nrow(data)){
+            if((grepl("tackle", data[i, "description"])>0)|(grepl("end", data[i, "description"])>0)|(grepl("up the middle", data[i, "description"])>0)|(grepl("guard", data[i, "description"])==1)&(grepl("scramble", data[i, "description"])==0)){
+                  data[i,"pType"] <- "Run"
+            } else if((grepl("pass", data[i, "description"])>0)|(grepl("sacked", data[i, "description"])>0)|(grepl("scramble", data[i, "description"])>0)){
+                  data[i,"pType"] <- "Pass"
+            } 
       }
+      return(data)
+}
+
+addConvert <- function(data){
       
-      data <- classifyNFLData(data)
+      data[,"convert"] <- FALSE
+      
+      for(i in 1:(nrow(data) - 1)){
+            
+            if((((data[i + 1,"down"] == 1) | (data[i + 1, "offscore"] > data[i, "offscore"])) & (data[i, "off"] == data[i + 1, "off"]) & (data[i,"down"]) != 0)|((data[i+2, "offscore"]-data[i, "offscore"]==7)&(data[i, "off"]==data[i+2, "off"]))){
+                  data[i,"convert"] <- TRUE
+            }
+      }
 }
